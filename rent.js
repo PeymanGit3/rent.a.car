f876836d3b47b133bbbedef49c9a37834b24c4b2
Pheiman Isayev <peymanisayev13@gmail.com>
2026-03-06 20:19:34 +0400

    new adds

--- rent.js
+++ rent.js
@@ -24,3 +24,121 @@
     });
 
   });
+
+// Registration form submission
+ const form = document.getElementById("registerForm")
+
+  form.addEventListener("submit", async function(e){
+
+    e.preventDefault()
+
+    const user = {
+
+      phoneNumber: document.getElementById("phone").value,
+      password: document.getElementById("password").value,
+      email: document.getElementById("email").value,
+      firstName: document.getElementById("name").value,
+      lastName: document.getElementById("surname").value,
+      role: "User"
+
+    }
+
+    try{
+
+    const response = await fetch(
+      "https://rentcar.stepprojects.ge/api/Users/register",
+      {
+          method: "POST",
+
+          headers:{
+          "Content-Type":"application/json"
+          },
+
+          body: JSON.stringify(user)
+
+      })
+
+      const data = await response.json()
+
+      console.log(data)
+
+    }catch(error){
+
+      console.log(error)
+
+    }
+
+})
+
+// Success message
+
+const congratulate = document.getElementById("registerForm")
+
+congratulate.addEventListener("submit", async function(e){
+
+e.preventDefault()
+
+const user = {
+
+phoneNumber: document.getElementById("phone").value,
+password: document.getElementById("password").value,
+email: document.getElementById("email").value,
+firstName: document.getElementById("name").value,
+lastName: document.getElementById("surname").value,
+role: "User"
+
+}
+
+try{
+
+const response2 = await fetch(
+"https://rentcar.stepprojects.ge/api/Users/register",
+{
+method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
+body: JSON.stringify(user)
+})
+
+const text = await response2.text()
+
+console.log(text)
+
+// Passing to Login page 
+if(response2.ok){
+
+document.getElementById("registerContainer").style.display = "none"
+
+document.getElementById("successMessage").style.display = "block"
+
+if(response2.ok){
+
+document.getElementById("registerContainer").style.display = "none"
+
+document.getElementById("successMessage").style.display = "block"
+
+setTimeout(()=>{
+window.location.href = "login.html"
+},2000)
+
+}
+
+// Success message & Passing to Login page 
+}
+
+}catch(error){
+
+console.log(error)
+
+}
+
+})
+
+
+
+
+
+
+
+
